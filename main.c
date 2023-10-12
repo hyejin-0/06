22f3e72d58b0b027263e84e511f7a451812bd75c
hyejin-0 <hj111800@naver.com>
2023-10-12 16:14:26 +0900

    3

--- main.c
+++ main.c
@@ -3,15 +3,37 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func1(void) {
-	int x;
-	printf("func1 x is at %p\n", &x);
+int sumTwo(int a, int b)
+{
+	int c = a + b;
+	return c;
 }
 
-int main(void) {
-	int x;
-	printf("main x is at %p\n", &x);
-	func1();
+int square(int n)
+{
+	return n*n;
+}
+
+int get_max(int x, int y)
+{
+	if (x > y)
+	return x;
+	
+	return y;
+}
+	
+int main(int argc, char *argv[])
+{
+	int a, b;
+	
+	a = 10;
+	b = 3;
+	printf("sumTwo result : %i\n", sumTwo(a, b));
+	
+	printf("square result : %i\n", square(a));
+	
+	printf("get_max result : %i\n", get_max(a, b));
+	
 	
 	return 0;
 }
